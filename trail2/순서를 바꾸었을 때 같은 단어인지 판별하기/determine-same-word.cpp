30dc55d9ef1da2a3e54fabda670c4356961fc552
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-05-28 12:09:05 +0900

    [Passed] 순서를 바꾸었을 때 같은 단어인지 판별하기 (14ms, 1MB)

---
+++ trail2/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string str1, str2;
+    cin >> str1 >> str2;
+    sort(str1.begin(), str1.end());
+    sort(str2.begin(), str2.end());
+    
+    if (str1.length() != str2.length()) {
+        cout << "No";
+        return 0;
+    }
+
+    for (int i = 0; i < str1.length(); ++i) 
+        if (str1[i] != str2[i]) {
+                cout << "No";
+                return 0;
+        }
+    cout << "Yes";
+    return 0;
+}
