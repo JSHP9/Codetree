9202bc57c86c0d9b1046e14ef182402ce7ac2c4e
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-05-31 15:00:23 +0900

    [Passed] 줄 세우기 2 (2ms, 0MB)

---
+++ trail2/줄 세우기 2/line-up-students-2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Human {
+public:
+    int h, w, n;
+    Human() {}
+    Human(int height, int weight, int num) {
+        this -> h = height;
+        this -> w = weight;
+        this -> n = num;
+    }
+};
+
+bool cmp(Human a, Human b) {
+    if (a.h == b.h) {
+        return a.w > b.w;
+    }
+    return a.h < b.h;
+
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int height, weight;
+    Human h[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> height >> weight;
+        h[i] = Human(height, weight, i + 1);
+    }
+    sort(h, h + n, cmp);
+    for (int i = 0; i < n; ++i) {
+        cout << h[i].h << " " << h[i].w << " " << h[i].n << "\n";
+    }
+    return 0;
+}
